05d3fe0139f1eeef6277f2c5332ec2a001a26280
Sarah Briggs <sbriggs@20001941-MacBook-Pro.local>
2019-05-29 13:08:30 -0500

    try registering component

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js'
 import App from './App'
+import Landing from './components/Landing'
 
 Vue.config.productionTip = false 
+Vue.component('Landing', Landing)
 
 /* eslint-disable no-new */
 new Vue({
